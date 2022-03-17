2b1057ef0592979eb48f494303414af032377a93
lukeyusuke <lukedev09@gmail.com>
2022-03-17 15:03:00 -0300

    Adicionando elementos via JavaScript
    Adiciona elementos no HTML via JavaScript

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,19 +4,28 @@ const listTasks = document.querySelector('.tasks-todo');
 
 const addTask = () => {
 
-    const addElementDiv = document.createElement('div');
-    addElementDiv.classList.add('task-item');
+    const addTaskItem = document.createElement('div');
+    addTaskItem.classList.add('task-item');
 
     const addParagraph = document.createElement('p');
     addParagraph.innerText = taskInput.value;
+
+    const addIconsBox = document.createElement('div');
+    addIconsBox.classList.add('icons-box');
+
+    const doItem = document.createElement('i');
+    doItem.classList.add('fa-solid');
+    doItem.classList.add('fa-check');
 
     const deleteItem = document.createElement('i');
     deleteItem.classList.add('fa-solid');
     deleteItem.classList.add('fa-trash-can');
 
-    listTasks.appendChild(addElementDiv);
-    addElementDiv.appendChild(addParagraph);
-    addElementDiv.appendChild(deleteItem);
+    listTasks.appendChild(addTaskItem);
+    addTaskItem.appendChild(addParagraph);
+    listTasks.appendChild(addIconsBox);
+    addIconsBox.appendChild(doItem);
+    addIconsBox.appendChild(deleteItem);
 }
 
 taskButton.addEventListener('click', () => addTask());
